5692540de4713c82c6bd1d6b7aaef328d768bc3c
Michael Graff <explorer@flame.org>
2022-08-29 14:44:36 -0500

    simple components

---
+++ util/util.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package util
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+)
+
+// TODO: I know, "util" is considered bad Go...
+
+func Traceback() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	return fmt.Sprintf("%s:%d %s\n", frame.File, frame.Line, frame.Function)
+}
+
+func Check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetEnvar(name string, defaultValue string) string {
+	value, found := os.LookupEnv(name)
+	if !found {
+		return defaultValue
+	}
+	return value
+}
---
+++ version/version.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package version
+
+import (
+	"log"
+
+	"github.com/OpsMx/go-app-base/util"
+)
+
+// Versions can be set via environment variables at run-time, or
+// can be linker-patched directly during the "go build" step.
+// Envars will override the variables defined here or patched.
+//
+// The expectation is that the docker container used in production will
+// have these set.  However, if we want to support containers and
+// native code, we may want to patch instead.
+//
+// Patching would also be required if more than one app runs in the same
+// container.
+var (
+	gitBranch = "dev"
+	gitHash   = "dev"
+)
+
+func findGitBranch() string {
+	return util.GetEnvar("GIT_BRANCH", gitBranch)
+}
+
+func findGitHash() string {
+	return util.GetEnvar("GIT_HASH", gitHash)
+}
+
+func ShowGitInfo() {
+	log.Printf("GIT Version: %s@%s", findGitBranch(), findGitHash())
+}
