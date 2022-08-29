f576856ac00db0074b475364f23055b4c55623bc
Michael Graff <explorer@flame.org>
2022-08-29 16:20:22 -0500

    test more

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/OpsMx/go-app-base
 go 1.19
 
 require (
+	github.com/stretchr/testify v1.8.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.34.0
 	go.opentelemetry.io/otel v1.9.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.9.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.9.0
@@ -10,8 +12,13 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/otel/metric v0.31.0 // indirect
 	go.opentelemetry.io/otel/trace v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ httputil/errors.go
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package httputil
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// StatusCodeOK returns true if the error code provided is between
+// 200 and 2999, inclusive.
+func StatusCodeOK(statusCode int) bool {
+	return statusCode >= 200 && statusCode <= 299
+}
+
+// HTTPError defines a simple struct to return JSON formatted error
+// messages.
+type HttpError struct {
+	Status string      `json:"status,omitempty" yaml:"status,omitempty"`
+	Code   int         `json:"code,omitempty" yaml:"code,omitempty"`
+	Error  interface{} `json:"error,omitempty" yaml:"error,omitempty"`
+}
+
+// SetError returns a JSON error message with a Status field set to 'error',
+// a 'code' set to the provided code, and the 'error' set to the provided
+// content.
+//
+// Prior to calling, nothing should be written to the writer, and afterwards
+// nothing should be written.
+//
+// The content-type will be set to application/json.
+func SetError(w http.ResponseWriter, statusCode int, message interface{}) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(statusCode)
+	m := HttpError{Status: "error", Code: statusCode, Error: message}
+	d, err := json.Marshal(m)
+	if err != nil {
+		return
+	}
+	w.Write(d)
+}
